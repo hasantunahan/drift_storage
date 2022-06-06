95d7911886e2a1e2968410a89d8484a7a4cdf4e1
hasantunahan <hasantunahan12345@gmail.com>
2022-06-06 10:55:07 +0300

    encrpty class delete

--- lib/drift/database.dart
+++ lib/drift/database.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:drift_example/drift_model/vehicle_table.dart';
-import 'package:drift_example/encrpty/encrpty.dart';
 import 'package:encrypt/encrypt.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
